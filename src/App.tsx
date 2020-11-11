fbdfa58f49d3c0cfbeaf1fc596fde54361eb823f
Felipe Genuino <design@felipegenuino.com>
2020-11-11 20:35:03 -0300

    Start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ 
+import "./pages/landing.css"
+
+import logoImg from './assets/Logo.svg'
+
+interface TitleProps{
+  text: string
+}
+function Title(props:TitleProps) {
+  return(
+    <h1> {props.text} </h1>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <a href="#" className='enter-app'>
+          x
         </a>
-      </header>
+
+      </div> 
     </div>
   );
 }
